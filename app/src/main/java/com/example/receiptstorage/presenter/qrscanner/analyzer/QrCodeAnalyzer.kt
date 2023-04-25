847c540d4dfdda676e48e6f0a46ba343bd230ef0
olegmac <olegprivetfl@gmail.com>
2023-04-25 08:38:40 +0300

    Improvements to the qr code scanner

--- app/src/main/java/com/example/receiptstorage/presenter/qrscanner/analyzer/QrCodeAnalyzer.kt
+++ app/src/main/java/com/example/receiptstorage/presenter/qrscanner/analyzer/QrCodeAnalyzer.kt
@@ -1,8 +1,14 @@
 package com.example.receiptstorage.presenter.qrscanner.analyzer
 
+import android.annotation.SuppressLint
 import android.graphics.ImageFormat
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions.Builder
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.DecodeHintType
@@ -11,54 +17,44 @@ import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import timber.log.Timber
 import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.SECONDS
 
 class QrCodeAnalyzer(
-    private val onQrCodeScanned: (String) -> Unit
-): ImageAnalysis.Analyzer {
+    private val onQrCodeScanned: (barcodes: List<Barcode>) -> Unit
+) : ImageAnalysis.Analyzer {
 
-    private val supportedQrFormats = listOf(
-        ImageFormat.YUV_420_888,
-        ImageFormat.YUV_422_888,
-        ImageFormat.YUV_444_888,
-    )
+    private var lastAnalyzedTimeStamp = 0L
 
+    @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(image: ImageProxy) {
-        if (image.format !in supportedQrFormats) return
+        val currentTimestamp = System.currentTimeMillis()
+        if (currentTimestamp - lastAnalyzedTimeStamp >= SECONDS.toMillis(1)) {
+            image.image?.let { imageToAnalyze ->
+                val options = Builder()
+                    .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+                    .build()
+                val barcodeScanner = BarcodeScanning.getClient(options)
+                val imageToProcess = InputImage.fromMediaImage(imageToAnalyze, image.imageInfo.rotationDegrees)
 
-        val bytes = image.planes.first().buffer.toByteArray()
-        val source = PlanarYUVLuminanceSource(
-            bytes,
-            image.width,
-            image.height,
-            0,
-            0,
-            image.width,
-            image.height,
-            false
-        )
-        val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
-        try {
-            val result = MultiFormatReader().apply {
-                setHints(
-                    mapOf(
-                        DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
-                            BarcodeFormat.QR_CODE
-                        )
-                    )
-                )
-            }.decode(binaryBitmap)
-            onQrCodeScanned(result.text)
-        } catch (e: Exception) {
-            Timber.e(e)
-        } finally {
+                barcodeScanner.process(imageToProcess)
+                    .addOnSuccessListener { barcodes ->
+                        if (barcodes.isNotEmpty()) {
+                            onQrCodeScanned(barcodes)
+                        } else {
+                            Timber.d("analyze: No barcode Scanned")
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Timber.d("BarcodeAnalyser: Something went wrong $exception")
+                    }
+                    .addOnCompleteListener {
+                        image.close()
+                    }
+            }
+            lastAnalyzedTimeStamp = currentTimestamp
+        } else {
             image.close()
-        }
-    }
-
-    private fun ByteBuffer.toByteArray(): ByteArray {
-        rewind()
-        return ByteArray(remaining()).also {
-            get(it)
         }
     }
 }
--- app/src/main/java/com/example/receiptstorage/presenter/qrscanner/view/QrCodeScanView.kt
+++ app/src/main/java/com/example/receiptstorage/presenter/qrscanner/view/QrCodeScanView.kt
@@ -7,6 +7,8 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.camera.view.PreviewView.ImplementationMode.COMPATIBLE
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -30,6 +32,10 @@ import com.example.receiptstorage.presenter.qrscanner.action.QrScannerAction
 import com.example.receiptstorage.presenter.qrscanner.analyzer.QrCodeAnalyzer
 import com.example.receiptstorage.presenter.qrscanner.viewmodel.QrScannerViewModel
 import com.example.receiptstorage.presenter.ui.theme.RsTheme
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import timber.log.Timber
 
 @Composable
 fun QrCodeScanView(
@@ -40,10 +46,7 @@ fun QrCodeScanView(
     val lifecycleOwner = LocalLifecycleOwner.current
     val screenSize = LocalConfiguration.current
     val cameraHeight by remember { mutableStateOf((screenSize.screenHeightDp / 3)) }
-    val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(context)
-    }
-    var isScan by remember { mutableStateOf(false) }
+    var preview by remember { mutableStateOf<Preview?>(null) }
     Text(
         modifier = modifier
             .fillMaxWidth()
@@ -55,38 +58,50 @@ fun QrCodeScanView(
     )
     AndroidView(
         factory = { context ->
-            val previewView = PreviewView(context).apply {
+            PreviewView(context).apply {
                 layoutParams = ViewGroup.LayoutParams(cameraHeight, cameraHeight)
+                scaleType = PreviewView.ScaleType.FILL_CENTER
+                implementationMode = COMPATIBLE
             }
-            val preview = Preview.Builder().build()
-            val selector = CameraSelector.Builder()
+        },
+        update = { previewView ->
+            val cameraSelector: CameraSelector = CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build()
-            preview.setSurfaceProvider(previewView.surfaceProvider)
-            val imageAnalysis = ImageAnalysis.Builder()
-                .setTargetResolution(Size(cameraHeight, cameraHeight))
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-            imageAnalysis.setAnalyzer(
-                ContextCompat.getMainExecutor(context),
-                QrCodeAnalyzer { result ->
-                    if (isScan) return@QrCodeAnalyzer
+            val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+            val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                ProcessCameraProvider.getInstance(context)
 
-                    isScan = true
-                    viewModel.setAction(QrScannerAction.FetchData(result))
+            cameraProviderFuture.addListener({
+                preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
                 }
-            )
-            try {
-                cameraProviderFuture.get().bindToLifecycle(
-                    lifecycleOwner,
-                    selector,
-                    preview,
-                    imageAnalysis
-                )
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            previewView
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                val barcodeAnalyser = QrCodeAnalyzer { barcodes ->
+                    barcodes.forEach { barcode ->
+                        val barcodeValue = barcode.rawValue ?: return@forEach
+                        viewModel.setAction(QrScannerAction.FetchData(barcodeValue))
+                    }
+                }
+                val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                    }
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageAnalysis
+                    )
+                } catch (e: Exception) {
+                    Timber.d("CameraPreview: ${e.localizedMessage}")
+                }
+            }, ContextCompat.getMainExecutor(context))
         },
         modifier = modifier.height(cameraHeight.dp)
     )
